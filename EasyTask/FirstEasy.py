20f12972ac0a554ea26c16cabdcb04e204619a9e
marmelad04ka <dimon2010a@yandex.ru>
2024-06-19 15:23:11 +0300

    add 2 task

--- EasyTask/FirstEasy.py
+++ EasyTask/FirstEasy.py
@@ -1,1 +1,9 @@
-1
+#1. https://leetcode.com/problems/score-of-a-string/description/
+def scoreOfString(self, s: str) -> int:
+    res = 0
+    for i in range(len(s) - 1):
+        res += abs(ord(s[i]) - ord(s[i + 1]))
+    return res
+#2. https://leetcode.com/problems/find-the-maximum-achievable-number/
+def theMaximumAchievableX(self, num: int, t: int) -> int:
+    return num + t + t
