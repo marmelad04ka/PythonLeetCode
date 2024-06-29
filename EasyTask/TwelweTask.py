599ae750891ae48cebbc14187aa29f3de7d5a285
marmelad04ka <dimon2010a@yandex.ru>
2024-06-29 09:50:17 +0300

    add 10 task

--- EasyTask/TwelweTask.py
+++ EasyTask/TwelweTask.py
@@ -59,8 +59,68 @@ def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
             res.append(i)
     return res
 
-#6.
-#7.
-#8.
-#9.
-#10.
+#6. https://leetcode.com/problems/intersection-of-two-arrays/description/
+def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    nums1 = set(nums1)
+    nums2 = set(nums2)
+    res = []
+    for i in nums1:
+        if i in nums2:
+            res.append(i)
+    return res
+
+#7. https://leetcode.com/problems/pascals-triangle/description/
+def generate(self, numRows: int) -> List[List[int]]:
+    if numRows == 1: return [[1]]
+    res = [[1],[1,1]]
+    k = 1
+    while numRows > 2:
+        mid = [1]
+        for i in range(len(res[k]) - 1):
+            mid.append(res[k][i] + res[k][i + 1])
+        mid.append(1)
+        res.append(mid)
+        k += 1
+        numRows -= 1
+    return res
+
+#8. https://leetcode.com/problems/find-the-k-or-of-an-array/description/
+def findKOr(self, nums: List[int], k: int) -> int:
+    res = ''
+    dir = {}
+    for i in range(len(nums)):
+        binStr = bin(nums[i])[:1:-1]
+        for ii in range(len(binStr)):
+            if ii in dir:
+                dir[ii] += int(binStr[ii])
+            else:
+                dir[ii] = int(binStr[ii])
+    for value in dir.keys():
+            if dir[value] >= k:
+                res += '1'
+            else: res += '0'
+    return int(res[::-1], 2)
+
+#9. https://leetcode.com/problems/count-operations-to-obtain-zero/description/
+def countOperations(self, num1: int, num2: int) -> int:
+    if num1 == 0 or num2 == 0: return 0
+    res = 0
+    while True:
+        if num1 == 0 or num2 == 0:
+            break
+        if num1 < num2:
+            num2 -= num1
+        else:
+            num1 -= num2
+        res += 1
+    return res
+
+#10. https://leetcode.com/problems/count-symmetric-integers/description/
+def countSymmetricIntegers(self, low: int, high: int) -> int:
+    res = 0
+    for i in range(low, high + 1):
+        if len(str(i))%2 != 0: continue
+        l = int(len(str(i))/2)
+        if sum(map(int, str(i)[:l])) == sum(map(int, str(i)[l:])):
+            res += 1
+    return res
