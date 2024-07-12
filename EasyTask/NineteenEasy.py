452c26610b73d413337a44e0c71440d6cfb6f6e0
marmelad04ka <dimon2010a@yandex.ru>
2024-07-12 19:38:38 +0300

    add 10 task

--- EasyTask/NineteenEasy.py
+++ EasyTask/NineteenEasy.py
@@ -40,9 +40,62 @@ def divideString(self, s: str, k: int, fill: str) -> List[str]:
         result.append(s[i: i + k])
     return result
 
-#5.
-#6.
-#7.
-#8.
-#9.
-#10.
+#5. https://leetcode.com/problems/apple-redistribution-into-boxes/description/
+def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
+    capacity.sort()
+    sumA = sum(apple)
+    count = 1
+    for i in range(len(capacity)-1, -1, -1):
+        if sumA - capacity[i] > 0:
+            sumA -= capacity[i]
+            count += 1
+        else: break
+    return count
+
+#6. https://leetcode.com/problems/reformat-date/description/
+def reformatDate(self, date: str) -> str:
+    month = ['0',"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    l1 = date.split(" ")
+    result = ''
+    result += l1[2] + '-'
+    if month.index(l1[1]) < 10: result += '0'
+    result += str(month.index(l1[1])) + '-'
+    if len(l1[0]) == 3: result += '0' + l1[0][:1]
+    else: result += l1[0][:2]
+    return result
+
+#7. https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/
+def maxDistance(self, colors: List[int]) -> int:
+    l1 = list(set(colors))
+    result = 0
+    for i in range(len(l1)):
+        for ii in range(len(colors) - 1, -1, -1):
+            if colors[ii] != l1[i]:
+                if ii - colors.index(l1[i]) > result: result = ii - colors.index(l1[i])
+                break
+    return result
+
+#8. https://leetcode.com/problems/divide-a-string-into-groups-of-size-k/description/
+def divideString(self, s: str, k: int, fill: str) -> List[str]:
+    result = []
+    lenS = len(s)
+    for i in range(lenS, lenS + k):
+        if i%k == 0: s += fill * (i - lenS)
+    for i in range(0, lenS, k):
+        result.append(s[i: i + k])
+    return result
+
+#9. https://leetcode.com/problems/calculate-digit-sum-of-a-string/description/
+def digitSum(self, s: str, k: int) -> str:
+    res = ''
+    while len(s) > k:
+        for i in range(0, len(s), k):
+            str1 = s[i: i + k]
+            res += str(sum(int(digit) for digit in str1))
+        s = res
+        res = ""
+    return s
+
+#10. https://leetcode.com/problems/sort-colors/description/
+def sortColors(self, nums: List[int]) -> None:
+    nums.sort()
