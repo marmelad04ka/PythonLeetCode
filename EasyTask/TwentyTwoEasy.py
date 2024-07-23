a6e59c9f1d2b2acbebf309942270c4041061d342
marmelad04ka <dimon2010a@yandex.ru>
2024-07-23 14:14:09 +0300

    add 3 task

--- EasyTask/TwentyTwoEasy.py
+++ EasyTask/TwentyTwoEasy.py
@@ -13,7 +13,16 @@ def largestOddNumber(self, num: str) -> str:
 def average(self, salary: List[int]) -> float:
     salary.sort()
     return sum(salary[1:len(salary) - 1])/len(salary[1:len(salary) - 1])
-#3.
+
+#3. https://leetcode.com/problems/valid-parentheses/description/
+def isValid(self, s: str) -> bool:
+    if len(s)%2 != 0: return False
+    res = ''
+    for i in s:
+        if i in '({[': res += i
+        elif len(res) != 0 and ((i == ')' and res[-1] == '(') or (i == ']' and res[-1] == '[') or (i == '}' and res[-1] == '{')): res = res[:-1]
+        else: return False
+    return res == ''
 #4.
 #5.
 #6.
