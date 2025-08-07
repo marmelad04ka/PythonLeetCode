858f546ab1656a118ff080c22b091c073a30f411
marmelad04ka <dimon2010a@yandex.ru>
2025-08-07 14:55:02 +0300

    682, 2733, 2089, 905, 1304, 3151, 657, 2716, 461, 2951

--- EasyTask/ElevenTask.py
+++ EasyTask/ElevenTask.py
@@ -1,5 +1,5 @@
 from typing import List
-#1. https://leetcode.com/problems/baseball-game/description/
+#682. https://leetcode.com/problems/baseball-game/description/
 def calPoints(self, operations: List[str]) -> int:
     res = []
     for i in operations:
@@ -13,7 +13,7 @@ def calPoints(self, operations: List[str]) -> int:
             res.append(int(i))
     return sum(res)
 
-#2. https://leetcode.com/problems/neither-minimum-nor-maximum/description/
+#2733. https://leetcode.com/problems/neither-minimum-nor-maximum/description/
 def findNonMinOrMax(self, nums: List[int]) -> int:
     nums.sort()
     res = -1
@@ -22,7 +22,7 @@ def findNonMinOrMax(self, nums: List[int]) -> int:
         break
     return res
 
-#3. https://leetcode.com/problems/find-target-indices-after-sorting-array/description/
+#2089. https://leetcode.com/problems/find-target-indices-after-sorting-array/description/
 def targetIndices(self, nums: List[int], target: int) -> List[int]:
     res = []
     nums.sort()
@@ -31,12 +31,12 @@ def targetIndices(self, nums: List[int], target: int) -> List[int]:
             res.append(i)
     return res
 
-#4. https://leetcode.com/problems/sort-array-by-parity/description/
+#905. https://leetcode.com/problems/sort-array-by-parity/description/
 def sortArrayByParity(self, nums: List[int]) -> List[int]:
     nums.sort(key = lambda x: x%2 != 0)
     return nums
 
-#5. https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/description/
+#1304. https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/description/
 def sumZero(self, n: int) -> List[int]:
     res = []
     for i in range(1, int(n/2) + 1):
@@ -45,22 +45,22 @@ def sumZero(self, n: int) -> List[int]:
     if n%2 != 0: res.append(0)
     return res
 
-#6. https://leetcode.com/problems/special-array-i/description/
+#3151. https://leetcode.com/problems/special-array-i/description/
 def isArraySpecial(self, nums: List[int]) -> bool:
     for i in range(len(nums) - 1):
         if nums[i]%2 == 0 and nums[i + 1]%2 == 0 or nums[i]%2 != 0 and nums[i + 1]%2 != 0:
             return False
     return True
 
-#7. https://leetcode.com/problems/robot-return-to-origin/description/
+#657. https://leetcode.com/problems/robot-return-to-origin/description/
 def judgeCircle(self, moves: str) -> bool:
     return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
 
-#8. https://leetcode.com/problems/minimize-string-length/description/
+#2716. https://leetcode.com/problems/minimize-string-length/description/
 def minimizedStringLength(self, s: str) -> int:
     return len(list(set(s)))
 
-#9. https://leetcode.com/problems/hamming-distance/description/
+#461. https://leetcode.com/problems/hamming-distance/description/
 def hammingDistance(self, x: int, y: int) -> int:
     binX = str(bin(x)[2:])
     binY = str(bin(y)[2:])
@@ -79,7 +79,7 @@ def hammingDistance(self, x: int, y: int) -> int:
             res += 1
     return res
 
-#10. https://leetcode.com/problems/find-the-peaks/description/
+#2951. https://leetcode.com/problems/find-the-peaks/description/
 def findPeaks(self, mountain: List[int]) -> List[int]:
     res = []
     for i in range(1, len(mountain) - 1):
