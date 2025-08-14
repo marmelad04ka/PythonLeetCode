56e00ad7ed0ce62049533c5fd1df267d37ecb17e
marmelad04ka <dimon2010a@yandex.ru>
2025-08-14 13:32:39 +0300

    58, 67, 70, 125, 190, 217, 231, 1, 2264

--- EasyTask/TwentyNine.py
+++ EasyTask/TwentyNine.py
@@ -1,4 +1,7 @@
 #58. https://leetcode.com/problems/length-of-last-word/description/
+from typing import List
+
+
 def lengthOfLastWord(self, s: str) -> int:
     return len(s.split()[-1])
 
@@ -14,10 +17,50 @@ def climbStairs(self, n: int) -> int:
         second += first
         first = second - first
     return second
-#1.
-#1.
-#1.
-#1.
-#1.
-#1.
+
+#125. https://leetcode.com/problems/valid-palindrome/description/
+def isPalindrome(self, s: str) -> bool:
+    alph_s = ''
+    for i in s.lower():
+        if i in 'qwertyuiopasdfghjklzxcvbnm0123456789':
+            alph_s += i
+    return alph_s == alph_s[::-1]
+
+#190. https://leetcode.com/problems/reverse-bits/description/
+def reverseBits(self, n: int) -> int:
+    binary = bin(n)[2:]
+    padded = binary.zfill(32)
+    reversed_binary = padded[::-1]
+    return int(reversed_binary, 2)
+
+#217. https://leetcode.com/problems/contains-duplicate/description/
+def containsDuplicate(self, nums: List[int]) -> bool:
+    seen = set()
+    for x in nums:
+        if x in seen:
+            return True
+        seen.add(x)
+    return False
+
+#231. https://leetcode.com/problems/power-of-two/description/
+def isPowerOfTwo(self, n: int) -> bool:
+    return n > 0 and (n & (n - 1)) == 0
+
+#1. https://leetcode.com/problems/two-sum/description/
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+
+#2264. https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/
+def largestGoodInteger(self, num: str) -> str:
+    result = ''
+    for i in range(len(num) - 2):
+        substring = num[i:i+3]
+        if substring[0] == substring[1] == substring[2]:
+            if substring > result: result = substring
+    return result
 #1.
