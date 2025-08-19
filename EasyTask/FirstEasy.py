ea1bcd4ad6f29016379055ddcc344945c22eda24
marmelad04ka <dimon2010a@yandex.ru>
2025-08-19 15:17:22 +0300

    3110, 2769, 1929, 1920, 3146, 2469, 1108, 2011, 1512, 2894

--- EasyTask/FirstEasy.py
+++ EasyTask/FirstEasy.py
@@ -1,33 +1,33 @@
 from typing import List
-#1. https://leetcode.com/problems/score-of-a-string/description/
+#3110. https://leetcode.com/problems/score-of-a-string/description/
 def scoreOfString(self, s: str) -> int:
     res = 0
     for i in range(len(s) - 1):
         res += abs(ord(s[i]) - ord(s[i + 1]))
     return res
 
-#2. https://leetcode.com/problems/find-the-maximum-achievable-number/
+#2769. https://leetcode.com/problems/find-the-maximum-achievable-number/
 def theMaximumAchievableX(self, num: int, t: int) -> int:
     return num + t + t
 
-#3. https://leetcode.com/problems/concatenation-of-array/description/
+#1929. https://leetcode.com/problems/concatenation-of-array/description/
 def getConcatenation(self, nums: List[int]) -> List[int]:
     return nums + nums
 
-#4. https://leetcode.com/problems/build-array-from-permutation/description/
+#1920. https://leetcode.com/problems/build-array-from-permutation/description/
 def buildArray(self, nums: List[int]) -> List[int]:
     res = []
     for i in range(len(nums)):
         res.append(nums[nums[i]])
     return res
-#5. https://leetcode.com/problems/permutation-difference-between-two-strings/description/
+#3146. https://leetcode.com/problems/permutation-difference-between-two-strings/description/
 def findPermutationDifference(self, s: str, t: str):
     res = 0
     for i in range(len(s)):
         res += abs(s.find(s[i]) - t.find(s[i]))
     return res
 
-#6. https://leetcode.com/problems/convert-the-temperature/description/
+#2469. https://leetcode.com/problems/convert-the-temperature/description/
 def convertTemperature(self, celsius: float):
     res = []
     k = celsius + 273.15
@@ -36,11 +36,11 @@ def convertTemperature(self, celsius: float):
     res.append(f)
     return res
 
-#7. https://leetcode.com/problems/defanging-an-ip-address/description/
+#1108. https://leetcode.com/problems/defanging-an-ip-address/description/
 def defangIPaddr(self, address: str):
     return address.replace('.', '[.]')
 
-#8. https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
+#2011. https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
 def finalValueAfterOperations(self, operations: List[str]) -> int:
     res = 0
     for i in operations:
@@ -48,7 +48,7 @@ def finalValueAfterOperations(self, operations: List[str]) -> int:
         else: res += 1
     return res
 
-#9. https://leetcode.com/problems/number-of-good-pairs/description/
+#1512. https://leetcode.com/problems/number-of-good-pairs/description/
 def numIdenticalPairs(self, nums: List[int]) -> int:
     res = 0
     for i in range(len(nums)):
@@ -57,7 +57,7 @@ def numIdenticalPairs(self, nums: List[int]) -> int:
                 res += 1
     return res
 
-#10. https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+#2894. https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
 def differenceOfSums(self, n: int, m: int) -> int:
         nonDi = sum(i for i in range(1, n+1) if i % m != 0)
         Di = sum(i for i in range(1, n+1) if i % m == 0)
